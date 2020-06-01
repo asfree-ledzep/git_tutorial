ea9af585e7bc25e9744c8f9f410c2e9561ece6b1
asfree <asfree@naver.com>
2020-06-01 20:37:57 +0900

    다른 라인추가 작업

--- HelloJava.java
+++ HelloJava.java
@@ -4,7 +4,8 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("추가내용 ");
 	}
 
 }
